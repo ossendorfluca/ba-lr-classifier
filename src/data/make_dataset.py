b0cb705b7b912bdfc240422c1d9615bf5d72202a
ossendorfluca <luca@oausb.de>
2024-01-05 10:25:47 +0100

    feature engineering

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,23 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import bibtexparser
+import pandas as pd
+
+with open('../../data/external/records.bib') as bibtex_file:
+    bib_database = bibtexparser.load(bibtex_file)
+
+records_df = pd.DataFrame(bib_database.entries)
+records_df.dropna(subset="literature_review", inplace=True)
+
+top_8 = ["European Journal of Information Systems", "Information System Journal", "Information System Research", "Journal of AIS", "Journal of Information Technology", "Journal of MIS", "Journal of Strategic Information Systems", "MIS Quarterly"]
+
+records_top_8 = records_df.loc[records_df['journal'].isin(top_8)]
+
+records_top_8.drop(['colrev.dblp.dblp_key', 'colrev_pdf_id', 'colrev_data_provenance', 'colrev_masterdata_provenance', 'colrev_status', 'colrev_origin', 'colrev.semantic_scholar.id', 'pdf_processed', 'prescreen_exclusion', 'note', 'fulltext', 'link', 'file', 'cited_by', 'screening_criteria', 'ID'], axis=1, inplace=True)
+records_top_8.reset_index(inplace=True, drop=True)
+records_top_8["literature_review"].replace(to_replace="yes", value=1, inplace=True)
+records_top_8["literature_review"].replace(to_replace="no", value=0, inplace=True)
+records_top_8.astype({"literature_review": int})
+records_top_8.to_csv("../../data/interim/data.csv")
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
